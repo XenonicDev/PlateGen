ed80a18661b89bb357fb83f937e3a9c3aaf19a22
XenonicDev <andrewdepke@gmail.com>
2018-03-02 23:22:15 -0700

    Added Templates for Searching/Removing Plates

--- PlateGen/PlateGen.py
+++ PlateGen/PlateGen.py
@@ -26,11 +26,16 @@ def GetOldBoilerplates(TreeRoot):
 def FindMatches(SearchDirectory, FileExpression):
     return glob.glob(SearchDirectory + "/**/" + FileExpression, recursive=True)
 
-def AddPlate(FileName, PlateBlock):
-    with open(FileName, "r+") as File:
-        Content = File.read()
-        File.seek(0, 0)
-        File.write(PlateBlock.rstrip("\r\n") + "\n" + Content)
+def RemovePlate(FileHandle, LineStart, LineEnd):
+    print()  # Placeholder
+
+def ContainsPlate(FileHandle, PlateBlock):
+    return False
+
+def AddPlate(FileHandle, PlateBlock):
+    Content = FileHandle.read()
+    FileHandle.seek(0, 0)
+    FileHandle.write(PlateBlock.rstrip("\r\n") + "\n" + Content)
 
 ConfigTree = ElementTree.parse("Config.xml")
 ConfigRoot = ConfigTree.getroot()
@@ -38,4 +43,10 @@ ConfigRoot = ConfigTree.getroot()
 for Directory in GetDirectoryList(ConfigRoot):
     for FileExpression in GetFileList(ConfigRoot):
         for FileName in FindMatches(Directory, FileExpression):
-            AddPlate(FileName, GetBoilerplate(ConfigRoot))
+            with open(FileName, "r+") as File:
+                for OldPlate in GetOldBoilerplates(ConfigRoot):
+                    if ContainsPlate(File, OldPlate):
+                        RemovePlate(File, 0, OldPlate.count("\n"))  # TODO: Search for First Character, Don't Start at 0
+                if ContainsPlate(File, GetBoilerplate(ConfigRoot)):
+                    RemovePlate(File, 0, GetBoilerplate(ConfigRoot).count("\n"))  # TODO: Search for First Character, Don't Start at 0
+                AddPlate(File, GetBoilerplate(ConfigRoot))
