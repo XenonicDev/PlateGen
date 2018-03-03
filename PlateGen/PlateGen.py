ef6ba960a7bc1af3fec09e53c1ff89c46a25c256
XenonicDev <andrewdepke@gmail.com>
2018-03-02 21:27:23 -0700

    Work on File I/O Setting Up, Added Config File

--- PlateGen/PlateGen.py
+++ PlateGen/PlateGen.py
@@ -11,5 +11,14 @@ Arguments = Parser.parse_args()
 def FindFiles(SearchDirectory, FileTargets):
     return glob.glob(SearchDirectory + "/**/" + FileTargets, recursive=True)
 
-for File in FindFiles(Arguments.Directory, Arguments.Files):
-    print(File)
+def AddPlate(FileName):
+    try:
+        File = open(FileName, "r+")
+        File.close()
+    except Exception as Error:
+        print("File Error: " + repr(Error))
+
+    
+
+for FileName in FindFiles(Arguments.Directory, Arguments.Files):
+    AddPlate(FileName)
---
+++ Sample/Test.cpp
@@ -0,0 +1,10 @@
+/*
+	Another comment
+*/
+
+// A comment
+
+int main()
+{
+	// Some chunk of code
+}
