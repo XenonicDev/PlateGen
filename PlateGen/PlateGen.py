3cc988961bc9d95cd7266f9c927f7871b05175a9
XenonicDev <andrewdepke@gmail.com>
2018-03-02 23:58:21 -0700

    Work On Search/Remove Old Plates

--- PlateGen/PlateGen.py
+++ PlateGen/PlateGen.py
@@ -27,19 +27,29 @@ def GetOldBoilerplates(TreeRoot):
 def FindMatches(SearchDirectory, FileExpression):
     return glob.glob(SearchDirectory + "/**/" + FileExpression, recursive=True)
 
+def MakeFileHandle(FileName):
+    print("Made File Handle")
+    return open(FileName, "r+")
+
 def RemovePlate(FileHandle, FileName, BlockLastLine):
+    print("Removing Plate")
     FileHandle.seek(BlockLastLine, 0)
     with open(FileName + ".tmp", "w") as TmpFile:
-        for Line in FileHandle
+        for Line in FileHandle:
             TmpFile.write(Line)
+    FileHandle.close()
     # Swap Temp with Original
     os.remove(FileName)
     os.rename(FileName + ".tmp", FileName)
 
 def ContainsPlate(FileHandle, PlateBlock):
+    FileHandle.seek(0, 0)
+    if PlateBlock in FileHandle.read(PlateBlock.count("\n")):
+        return True
     return False
 
 def AddPlate(FileHandle, PlateBlock):
+    FileHandle.seek(0, 0)
     Content = FileHandle.read()
     FileHandle.seek(0, 0)
     FileHandle.write(PlateBlock.rstrip("\r\n") + "\n" + Content)
@@ -50,10 +60,13 @@ ConfigRoot = ConfigTree.getroot()
 for Directory in GetDirectoryList(ConfigRoot):
     for FileExpression in GetFileList(ConfigRoot):
         for FileName in FindMatches(Directory, FileExpression):
-            with open(FileName, "r+") as File:
-                for OldPlate in GetOldBoilerplates(ConfigRoot):
-                    if ContainsPlate(File, OldPlate):
-                        RemovePlate(File, FileName, OldPlate.count("\n"))
-                if ContainsPlate(File, GetBoilerplate(ConfigRoot)):
-                    RemovePlate(File, FileName, GetBoilerplate(ConfigRoot).count("\n"))
-                AddPlate(File, GetBoilerplate(ConfigRoot))
+            File = MakeFileHandle(FileName)
+            for OldPlate in GetOldBoilerplates(ConfigRoot):
+                if ContainsPlate(File, OldPlate):
+                    RemovePlate(File, FileName, OldPlate.count("\n"))
+                    File = MakeFileHandle(FileName)
+            if ContainsPlate(File, GetBoilerplate(ConfigRoot)):
+                RemovePlate(File, FileName, GetBoilerplate(ConfigRoot).count("\n"))
+                File = MakeFileHandle(FileName)
+            AddPlate(File, GetBoilerplate(ConfigRoot))
+            File.close()
