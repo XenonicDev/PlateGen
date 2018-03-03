a5ab144ef921598d3177a0b6388d71a902023b7a
XenonicDev <andrewdepke@gmail.com>
2018-03-02 22:32:33 -0700

    Added Basic Config XML Parsing

--- PlateGen/PlateGen.py
+++ PlateGen/PlateGen.py
@@ -1,11 +1,27 @@
 import sys
-import argparse
 import glob
+import xml.etree.ElementTree as ElementTree
 
-Parser = argparse.ArgumentParser()
-Parser.add_argument("Directory")
-Parser.add_argument("Files")
-Arguments = Parser.parse_args()
+def GetDirectoryList(TreeRoot):
+    Directories = []
+    for Element in TreeRoot[0]:
+        Directories.append(Element.text)
+    return Directories
+
+def GetFileList(TreeRoot):
+    Files = []
+    for Element in TreeRoot[1]:
+        Files.append(Element.text)
+    return Files
+
+def GetBoilerplate(TreeRoot):
+    return TreeRoot[2].text
+
+def GetOldBoilerplates(TreeRoot):
+    Plates = []
+    for Element in TreeRoot[3]:
+        Plates.append(Element.text)
+    return Plates
 
 def FindFiles(SearchDirectory, FileTargets):
     return glob.glob(SearchDirectory + "/**/" + FileTargets, recursive=True)
@@ -17,5 +33,17 @@ def AddPlate(FileName):
     except Exception as Error:
         print("File Error: " + repr(Error))
 
-for FileName in FindFiles(Arguments.Directory, Arguments.Files):
-    AddPlate(FileName)
+ConfigTree = ElementTree.parse("Config.xml")
+
+ConfigRoot = ConfigTree.getroot()
+
+for Iter in GetDirectoryList(ConfigRoot):
+    print("Directory Found: ", Iter)
+for Iter in GetFileList(ConfigRoot):
+    print("File Found: ", Iter)
+print("Boilerplate Block Found:\n", GetBoilerplate(ConfigRoot))
+for Iter in GetOldBoilerplates(ConfigRoot):
+    print("Old Boilerplate Blocks Found:\n", Iter)
+
+#for FileName in FindFiles(Arguments.Directory, Arguments.Files):
+#    AddPlate(FileName)
